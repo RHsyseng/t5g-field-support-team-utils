c9e7e824974e3669ee909800cd43ba1bdc388e70
Adhitya Logan <alogan@redhat.com>
2021-08-06 07:46:46 -0700

    added /live route that clears the cache and redirects to homepage

--- dashboard/src/t5gweb/ui.py
+++ dashboard/src/t5gweb/ui.py
@@ -20,3 +20,9 @@ def index():
     now = datetime.datetime.utcnow()
     new_comments = get_new_comments()
     return render_template('ui/index.html', new_cases=new_cases, now=now, new_comments=new_comments)
+
+@BP.route('/live')
+def live():
+    """Forces an update to the dashboard"""
+    cache.clear()
+    return redirect(url_for("ui.index"))
