16a949c2d0c87a8d37036b163a8c6962a73dcb0e
Adhitya Logan <alogan@redhat.com>
2021-08-26 13:04:47 -0700

    added plot to index and moved new updates to /updates

--- dashboard/src/t5gweb/ui.py
+++ dashboard/src/t5gweb/ui.py
@@ -14,15 +14,17 @@ from t5gweb.t5gweb import (
 
 BP = Blueprint('ui', __name__, url_prefix='/')
 
+
 @BP.route('/')
 @cache.cached(timeout=14400)
 def index():
     """list new cases"""
     new_cases = get_new_cases()
+    plot_data = plots()
+    y = list(plot_data.values())
     now = datetime.datetime.utcnow()
-    new_comments = get_new_comments()
-    new_cnv = get_cnv()
-    return render_template('ui/index.html', new_cases=new_cases, now=now, new_comments=new_comments, new_cnv = new_cnv)
+    return render_template('ui/index.html', new_cases=new_cases, values=y, now=now)
+
 
 @BP.route('/refresh')
 def refresh():
@@ -30,9 +32,12 @@ def refresh():
     cache.clear()
     return redirect(url_for("ui.index"))
 
-@BP.route('/plots')
-def plot():
+
+@BP.route('/updates')
+@cache.cached(timeout=14400)
+def updates():
     """Retrieves summary data and creates Chart.JS plot"""
-    plot_data = plots()
-    y = list(plot_data.values())
-    return render_template('ui/plot.html', values = y)
+    new_comments = get_new_comments()
+    new_cnv = get_cnv()
+    now = datetime.datetime.utcnow()
+    return render_template('ui/updates.html', now=now, new_comments=new_comments, new_cnv=new_cnv)
