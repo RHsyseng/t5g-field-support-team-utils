ed6ea414094cc27ed7cff47f68138298e9c8a292
dcritch <dcritch@gmail.com>
2022-03-14 15:22:07 -0400

    code cleanup, part 1

--- k8s/build/python3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-container=$(buildah from registry.redhat.io/ubi8/ubi-minimal)
-echo "building container with id $container"
-buildah config --label maintainer="David Critch <dcritch@gmail.com>" $container
-buildah run $container microdnf -y install python3-pip gcc redhat-rpm-config python3-devel
-buildah commit --format docker $container python3:latest
