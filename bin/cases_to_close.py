b1c496d2dfc26a0bc59ea4bf47952abdf4176bbc
dcritch <dcritch@gmail.com>
2022-03-21 16:27:21 -0400

    updating older scripts to use api

---
+++ bin/cases_to_close.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python -W ignore
+'''
+This script hits the API to check for cases that are closed but the associated card is still open
+'''
+import os
+import sys
+import requests
+
+def check_cases(api_url, case_tag):
+    '''
+    Compare cases to cards for a given tag
+    '''
+    cases = requests.get("{}/api/cases/{}".format(api_url, case_tag))
+    if cases.status_code == 200:
+        closed_cases = {c: d for (c, d) in cases.json().items() if d['status'] == 'Closed'}
+    else:
+        print("could not retrieve cases: {}".format(cases.status_code))
+        sys.exit(1)
+    cards = requests.get("{}/api/cards/{}".format(api_url, case_tag))
+    if cases.status_code == 200:
+        open_cards = {c: d for (c, d) in cards.json().items() if d['card_status'] != 'Done'}
+    else:
+        print("could not retrieve cards: {}".format(cards.status_code))
+        sys.exit(1)
+    for card in open_cards:
+        if open_cards[card]['case_number'] in closed_cases.keys():
+            print("https://issues.redhat.com/browse/{} : {} ({})".format(
+                card, open_cards[card]['case_number'], open_cards[card]['assignee']['displayName']))
+
+def main():
+    '''
+    sets some things and then runs check_cases
+    '''
+    api_url = os.environ.get('T5G_API')
+    if api_url is None:
+        print('No API URL specified. Please export T5G_API=<host> and try again')
+        sys.exit(1)
+
+    if len(sys.argv) == 2:
+        case_tag = sys.argv[1]
+    else:
+        sys.exit('no case tag specified')
+    print("searching {} for closed cases / open cards ({})".format(api_url, case_tag))
+    if case_tag in ['cnv', 'telco5g']:
+        check_cases(api_url, case_tag)
+    else:
+        sys.exit('invalid case tag specified: {}'.format(case_tag))
+
+if __name__ == '__main__':
+    main()
---
+++ bin/open_and_closed_report.py
@@ -0,0 +1,59 @@
+#! /usr/bin/python -W ignore
+
+'''
+Checks how many cases have been opened and closed in the last week
+'''
+import datetime
+import os
+import sys
+import requests
+
+
+def case_report(api_url, case_tag):
+    '''
+    Checks for newly opened and closed cases for a given tag
+    '''
+    cases = requests.get("{}/api/cases/{}".format(api_url, case_tag))
+    if cases.status_code == 200:
+        cases = cases.json()
+    else:
+        print("could not retrieve cases: {}".format(cases.status_code))
+        sys.exit(1)
+
+    today = datetime.date.today()
+    age = 7
+
+    closed_cases = {c: d for (c, d) in cases.items() if d['status'] == 'Closed' and
+    (today - datetime.datetime.strptime(d['last_update'], '%Y-%m-%dT%H:%M:%SZ').date()).days < age}
+
+    opened_cases = {c: d for (c, d) in cases.items() if d['status'] != 'Closed' and
+    (today - datetime.datetime.strptime(d['createdate'], '%Y-%m-%dT%H:%M:%SZ').date()).days < age}
+
+    print("{} cases closed in the past {} days:".format(len(closed_cases), age))
+    for case in closed_cases:
+        print("{}: {}".format(case, closed_cases[case]['problem']))
+    print("{} cases opened in the past {} days:".format(len(opened_cases), age))
+    for case in opened_cases:
+        print("{}: {}".format(case, opened_cases[case]['problem']))
+
+def main():
+    '''
+    sets some things and then runs check_cases
+    '''
+    api_url = os.environ.get('T5G_API')
+    if api_url is None:
+        print('No API URL specified. Please export T5G_API=<host> and try again')
+        sys.exit(1)
+
+    if len(sys.argv) == 2:
+        case_tag = sys.argv[1]
+    else:
+        sys.exit('no case tag specified')
+    print("searching {} for newly opened and closed cases ({})".format(api_url, case_tag))
+    if case_tag in ['cnv', 'telco5g']:
+        case_report(api_url, case_tag)
+    else:
+        sys.exit('invalid case tag specified: {}'.format(case_tag))
+
+if __name__ == '__main__':
+    main()
