8ee93acf55f1f8a026aa743e94033131226e5ba6
David Critch <dcritch@gmail.com>
2022-11-23 12:43:24 -0500

    update dependency versions; remove unused modules

--- dashboard/build/t5gweb.sh
+++ dashboard/build/t5gweb.sh
@@ -3,23 +3,32 @@
 set -ex
 
 IMAGE=t5gweb
+NS=t5g-web
 TAG=${1:-latest}
 PUSH=${2:-false}
-NS=t5g-web
+BASE=${3:-false}
 
-echo building $IMAGE:$TAG
-
-container=$(buildah from registry.access.redhat.com/ubi8/python-39)
-echo "building container with id $container"
-buildah config --label maintainer="David Critch <dcritch@redhat.com.com>" $container
-buildah copy $container ../src/ /srv/
-buildah config --workingdir /srv $container
-buildah run $container pip3 install .
-buildah run $container pip3 install pylint
-buildah run $container npm ci --prefix t5gweb/static
-buildah config --port 8080 $container
-buildah commit --format docker $container $IMAGE:$TAG
-
+if [[ $BASE == "true" ]]; then
+  container=$(buildah from registry.fedoraproject.org/fedora:37)
+  echo "building container with id $container"
+  buildah config --label maintainer="David Critch <dcritch@redhat.com.com>" $container
+  buildah run $container dnf -y install python3-pip gcc redhat-rpm-config python3-devel npm
+  buildah copy $container ../src/ /srv/
+  buildah config --workingdir /srv $container
+  buildah run $container pip3 install .
+  buildah run $container npm ci --prefix t5gweb/static
+  buildah config --port 8080 $container
+  buildah commit --format docker $container $IMAGE-base:$TAG
+  buildah tag $IMAGE-base:$TAG $IMAGE:$TAG
+else
+  container=$(buildah from localhost/$IMAGE-base:$TAG)
+  echo "building container with id $container"
+  buildah config --label maintainer="David Critch <dcritch@redhat.com.com>" $container
+  buildah copy $container ../src/ /srv/
+  buildah config --workingdir /srv $container
+  buildah config --port 8080 $container
+  buildah commit --format docker $container $IMAGE:$TAG
+fi
 
 if [[ $PUSH == "true" ]]; then
   REGISTRY=${REGISTRY:-$(oc get route/default-route -n openshift-image-registry -o json | jq -r .spec.host)}
@@ -27,6 +36,6 @@ if [[ $PUSH == "true" ]]; then
   buildah tag $IMAGE:$TAG $REGISTRY/$NS/$IMAGE:$TAG
   buildah login -u $(oc whoami) -p $(oc whoami -t) --tls-verify=false $REGISTRY
   buildah push --tls-verify=false $REGISTRY/$NS/$IMAGE:$TAG
-  oc scale --replicas=0 deployment/t5gweb -n $NS
-  oc scale --replicas=1 deployment/t5gweb -n $NS
+  oc delete pod -l app=t5gweb-tasks
+  for pod in $(oc get pods -l app=t5gweb-app --no-headers | awk '{print $1}'); do oc delete pod/$pod; sleep 15; done # rolling update
 fi
--- dashboard/src/setup.py
+++ dashboard/src/setup.py
@@ -2,22 +2,20 @@ from setuptools import find_packages, setup
 
 setup(
     name='t5gweb',
-    version='0.210603',
+    version='0.221121',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask==2.0.2',
-        'matplotlib==3.4.3',
+        'flask==2.2.2',
         'gunicorn==20.1.0',
-        'jira==3.1.1',
-        'requests==2.26.0',
-        'slack_sdk==3.11.2',
-        'flask_apscheduler==1.12.2',
-        'redis==4.1.4',
+        'jira==3.4.1',
+        'requests==2.28.1',
+        'slack_sdk==3.19.4',
+        'redis==4.3.4',
         'python_bugzilla==3.2.0',
-        'celery==5.2.3',
-        'smartsheet-python-sdk==2.105.1',
-        'flower==1.0.0'
+        'smartsheet-python-sdk==2.177.1',
+        'celery==5.2.7',
+        'flower==1.2.0'
     ],
 )
