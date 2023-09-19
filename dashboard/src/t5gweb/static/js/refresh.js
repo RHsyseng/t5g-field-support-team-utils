32dac01f5583839630463ab7f71748f153afcbe1
Adhitya Logan <alogan@redhat.com>
2023-09-19 14:51:40 -0700

    lint js files with prettier

--- dashboard/src/t5gweb/static/js/refresh.js
+++ dashboard/src/t5gweb/static/js/refresh.js
@@ -31,25 +31,27 @@
  * If refresh is in progress, display progress bar and call updatePercentage()
  * to continuously update it.
  */
- $(document).ready(function () {
-    $.ajax({
-        type: 'POST',
-        url: '/progress/status',
-        success: function (data, status, request) {
-            status_url = request.getResponseHeader('Location');
-            $.getJSON(status_url, function (data) {
-                if (data['state'] == 'PROGRESS') {
-                    $('#progressbar').empty();
-                    div = $('<div class="text-center text-white">Refresh in progress...</div> <div class="text-white progress"><div class="progress-bar bg-danger" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0%</div></div>');
-                    $('#progressbar').append(div);
-                    updatePercentage(status_url, $('#progressbar'));
-                }
-            });
-        },
-        error: function () {
-            alert('Unexpected error')
+$(document).ready(function () {
+  $.ajax({
+    type: "POST",
+    url: "/progress/status",
+    success: function (data, status, request) {
+      status_url = request.getResponseHeader("Location");
+      $.getJSON(status_url, function (data) {
+        if (data["state"] == "PROGRESS") {
+          $("#progressbar").empty();
+          div = $(
+            '<div class="text-center text-white">Refresh in progress...</div> <div class="text-white progress"><div class="progress-bar bg-danger" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0%</div></div>',
+          );
+          $("#progressbar").append(div);
+          updatePercentage(status_url, $("#progressbar"));
         }
-    });
+      });
+    },
+    error: function () {
+      alert("Unexpected error");
+    },
+  });
 });
 
 /**
@@ -58,20 +60,22 @@
  * to continuously update the progress bar.
  */
 function getBackground() {
-    $('#progressbar').empty();
-    div = $('<div class="text-center text-white">Refresh in progress...</div> <div class="text-white progress"><div class="progress-bar bg-danger" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0%</div></div>');
-    $('#progressbar').append(div);
-    $.ajax({
-        type: 'POST',
-        url: '/refresh',
-        success: function (data, status, request) {
-            status_url = request.getResponseHeader('Location');
-            updatePercentage(status_url, $('#progressbar'))
-        },
-        error: function () {
-            alert('Unexpected error')
-        }
-    })
+  $("#progressbar").empty();
+  div = $(
+    '<div class="text-center text-white">Refresh in progress...</div> <div class="text-white progress"><div class="progress-bar bg-danger" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0%</div></div>',
+  );
+  $("#progressbar").append(div);
+  $.ajax({
+    type: "POST",
+    url: "/refresh",
+    success: function (data, status, request) {
+      status_url = request.getResponseHeader("Location");
+      updatePercentage(status_url, $("#progressbar"));
+    },
+    error: function () {
+      alert("Unexpected error");
+    },
+  });
 }
 
 /**
@@ -79,32 +83,37 @@ function getBackground() {
  * every two seconds. Also displays the final result of the task.
  */
 function updatePercentage(status_url, div) {
-    $.getJSON(status_url, function (data) {
-        percent = parseInt(data['current'] * 100 / data['total']);
-        div.find(".progress-bar").css('width', percent + '%').attr('aria-valuenow', percent).text(percent + "%");
-        if (!('locked' in data)) {
-            if (data['state'] != 'PENDING' && data['state'] != 'PROGRESS') {
-                if ('result' in data) {
-                    // show result
-                    $(div).text(data['result']).css('color', 'white');
-                }
-                else {
-                    // something unexpected happened
-                    $(div).text(data['state'] + ", please try again.").css('color', 'white');
-                }
-            } else {
-                // rerun in 2 seconds
-                setTimeout(function () {
-                    updatePercentage(status_url, div);
-                }, 2000);
-            }
+  $.getJSON(status_url, function (data) {
+    percent = parseInt((data["current"] * 100) / data["total"]);
+    div
+      .find(".progress-bar")
+      .css("width", percent + "%")
+      .attr("aria-valuenow", percent)
+      .text(percent + "%");
+    if (!("locked" in data)) {
+      if (data["state"] != "PENDING" && data["state"] != "PROGRESS") {
+        if ("result" in data) {
+          // show result
+          $(div).text(data["result"]).css("color", "white");
+        } else {
+          // something unexpected happened
+          $(div)
+            .text(data["state"] + ", please try again.")
+            .css("color", "white");
         }
-    })
+      } else {
+        // rerun in 2 seconds
+        setTimeout(function () {
+          updatePercentage(status_url, div);
+        }, 2000);
+      }
+    }
+  });
 }
 
 /**
  * Start background refresh on button click
  */
 $(function () {
-    $('#refresh').click(getBackground);
+  $("#refresh").click(getBackground);
 });
--- dashboard/src/t5gweb/static/js/stats.js
+++ dashboard/src/t5gweb/static/js/stats.js
@@ -1,66 +1,65 @@
-
 // Hide all datasets in chart
-$('#hide-stats').click(function () {
-    statsChart.data.datasets.forEach((element, index) => {
-        statsChart.setDatasetVisibility(index, false)
-    });
-    statsChart.update();
+$("#hide-stats").click(function () {
+  statsChart.data.datasets.forEach((element, index) => {
+    statsChart.setDatasetVisibility(index, false);
+  });
+  statsChart.update();
 });
 
 // Show all datasets in chart
-$('#show-stats').click(function () {
-    statsChart.data.datasets.forEach((element, index) => {
-        statsChart.setDatasetVisibility(index, true)
-    });
-    statsChart.update();
+$("#show-stats").click(function () {
+  statsChart.data.datasets.forEach((element, index) => {
+    statsChart.setDatasetVisibility(index, true);
+  });
+  statsChart.update();
 });
 
 // First table requires natural sort, so initialize separately
 $(document).ready(function () {
-    $('#overall').DataTable({
-        "paging": false,
-        "info": false,
-        "searching": false,
-        columnDefs: [
-            { type: 'natural-nohtml', targets: 1 }
-        ]
-    });
+  $("#overall").DataTable({
+    paging: false,
+    info: false,
+    searching: false,
+    columnDefs: [{ type: "natural-nohtml", targets: 1 }],
+  });
 });
 
 // Sort Severities Correctly (https://datatables.net/examples/plug-ins/sorting_auto.html)
-$.fn.dataTable.ext.type.detect.unshift(
-    function (d) {
-        return d === 'Low' || d === 'Normal' || d === 'High' || d === 'Urgent' ?
-            'severity-grade' :
-            null;
-    }
-);
+$.fn.dataTable.ext.type.detect.unshift(function (d) {
+  return d === "Low" || d === "Normal" || d === "High" || d === "Urgent"
+    ? "severity-grade"
+    : null;
+});
 
-$.fn.dataTable.ext.type.order['severity-grade-pre'] = function (d) {
-    switch (d) {
-        case 'Low': return 1;
-        case 'Normal': return 2;
-        case 'High': return 3;
-        case 'Urgent': return 4;
-    }
-    return 0;
+$.fn.dataTable.ext.type.order["severity-grade-pre"] = function (d) {
+  switch (d) {
+    case "Low":
+      return 1;
+    case "Normal":
+      return 2;
+    case "High":
+      return 3;
+    case "Urgent":
+      return 4;
+  }
+  return 0;
 };
 
 // Sort severity table from Urgent -> Low
 $(document).ready(function () {
-    $('#severity').DataTable({
-        "paging": false,
-        "info": false,
-        "searching": false,
-        "order": [[0, "desc"]]
-    });
+  $("#severity").DataTable({
+    paging: false,
+    info: false,
+    searching: false,
+    order: [[0, "desc"]],
+  });
 });
 
 // Other tables
 $(document).ready(function () {
-    $('table.display').DataTable({
-        "paging": false,
-        "info": false,
-        "searching": false,
-    })
+  $("table.display").DataTable({
+    paging: false,
+    info: false,
+    searching: false,
+  });
 });
