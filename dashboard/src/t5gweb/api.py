a953376aaab9e3b04e13c14c1a68546e450d444c
Adhitya Logan <alogan@redhat.com>
2023-10-04 17:25:23 -0700

    fix formatting

--- dashboard/src/t5gweb/api.py
+++ dashboard/src/t5gweb/api.py
@@ -1,17 +1,9 @@
 """API endpoints for t5gweb"""
-import json
-
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from t5gweb.cache import (
-    get_bz_details,
-    get_cards,
-    get_case_details,
-    get_cases,
-    get_escalations,
-    get_issue_details,
-    get_watchlist,
-)
+from t5gweb.cache import (get_bz_details, get_cards, get_case_details,
+                          get_cases, get_escalations, get_issue_details,
+                          get_watchlist)
 from t5gweb.libtelco5g import generate_stats, redis_get
 from t5gweb.utils import set_cfg
 
