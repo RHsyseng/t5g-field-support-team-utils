e2f38c3fa116c31230d8fc000f45d8ae92b2302b
Adhitya Logan <adhityalogan@gmail.com>
2025-05-07 13:01:58 -0700

    Move create_app() into separate file

--- dashboard/src/app.py
+++ dashboard/src/app.py
@@ -4,7 +4,7 @@ import os
 
 from t5gweb import create_app
 
-app = create_app()
+app = create_app.create_app()
 if __name__ == "__main__":
     SVC_PORT = int(os.environ.get("SVC_PORT", 8080))
     app.run(debug=True, host="0.0.0.0", port=SVC_PORT)
