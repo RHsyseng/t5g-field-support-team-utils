abb69821a718d251a86ca7f24ba73c4b8b15bc71
Adrien Luneau <aluneau@redhat.com>
2025-05-16 15:30:01 +0200

    adding portal comment class

--- dashboard/src/t5gweb/models.py
+++ dashboard/src/t5gweb/models.py
@@ -1,4 +1,5 @@
 # models.py - Define database models
+from typing import cast
 from t5gweb.db import Base  # Import Base from db.py
 from sqlalchemy import Column, Integer, String, Date, FetchedValue
 
@@ -18,3 +19,25 @@ class Case(Base):
     product = Column(String)
     product_version = Column(String)
     fe_jira_card = Column(String, unique=True)
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    case_number = Column(String, nullable=False)
+    created_date = Column(Date, nullable=False)  # Same as in Case
+
+    author = Column(String, nullable=False)
+    comment_text = Column(Text, nullable=False)
+    commented_at = Column(Date, nullable=False)
+
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ['case_number', 'created_date'],
+            ['cases.case_number', 'cases.created_date'],
+            ondelete='CASCADE'
+        ),
+    )
+
+    case = relationship("Case", backref="comments")
+
