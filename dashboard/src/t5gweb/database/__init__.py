223e1552a5a920307d1c2151debfdcc4eb511f5a
bpokorad <bpokorad@redhat.com>
2025-08-06 16:45:35 +0100

    removes reference to unused method

--- dashboard/src/t5gweb/database/__init__.py
+++ dashboard/src/t5gweb/database/__init__.py
@@ -4,7 +4,7 @@ This module provides database functionality including:
 - Database models (Case, Comment, JiraCard, JiraComment)
 - Session management (engine, SessionLocal, Base)
 - Database operations (load_cases_postgres, load_jira_cards_postgres)
-- Utility functions (get_db, create_postgres_tables)
+- Utility functions (create_postgres_tables)
 """
 
 # Import database models
@@ -14,16 +14,14 @@ from .models import Case, Comment, JiraCard, JiraComment
 from .operations import load_cases_postgres, load_jira_cards_postgres
 
 # Import session management components
-from .session import Base, create_postgres_tables, db_config  # , get_db
+from .session import Base, create_postgres_tables, db_config
 
 # Export all components that were previously available from postgres_db.py
 __all__ = [
     # Session management
     "Base",
-    # "engine",
     "db_config",
     "create_postgres_tables",
-    # "get_db",
     # Models
     "Case",
     "Comment",
